f7b991932ba43fa7548ff4be2d27ca5b61e451a2
Nicholay Kuropatkin <nictaporuk@yandex.ru>
2016-11-06 21:34:34 +0200

    lesson 19 was complete

--- src/comments/api/serializers.py
+++ src/comments/api/serializers.py
@@ -2,14 +2,51 @@ from rest_framework.serializers import (
     ModelSerializer,
     HyperlinkedIdentityField,
     SerializerMethodField,
+    ValidationError
 )
-
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from comments.models import Comment
 
 comment_detail_url = HyperlinkedIdentityField(
     view_name='comments-api:thread',
     lookup_field='id',
 )
+
+
+def create_comment_serializer(model_type='post', slug=None, parent_id=None):
+    class CommentCreateSerializer(ModelSerializer):
+        class Meta:
+            model = Comment
+            fields = [
+            'id',
+            'parent_id',
+            'content',
+            'timestamp',
+            ]
+
+        def __init__(self, *args, **kwargs):
+            self.model_type = model_type
+            self.slug = slug
+            self.parent_obj = None
+            if self.parent_id:
+                parent_qs = Comment.objects.filter(id=parent_id)
+                if parent_qs.exists() and parent_qs.count == 1:
+                    self.parent_obj = parent_qs.first()
+            return super(CommentCreateSerializer, self).__init__(*args, **kwargs)
+
+        def validate(self, data):
+            model_type = self.model_type
+            model_qs = ContentType.objects.filter(model=model_type)
+            if not model_qs.exists() or model_qs.count() !=1:
+                raise ValidationError("This is not a valid content type")
+
+            SomeModel = model_qs.first().model_class()
+            obj_qs = SomeModel.objects.filter(slug=self.slug)
+            if not obj_qs.exists() or obj_qs.count != 1:
+                raise ValidationError("This is not a valid slug for this content type")
+
+    return CommentCreateSerializer
 
 class CommentSerializer(ModelSerializer):
     # url = comment_detail_url
